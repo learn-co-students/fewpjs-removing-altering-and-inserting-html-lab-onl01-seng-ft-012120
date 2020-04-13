507ea9697421dc844a8b050e132450dba5c76bdb
oliverfeher <oliver92.feher@gmail.com>
2020-04-13 03:56:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("main")
+main.parentNode.removeChild(main);
+
+
+let body = document.querySelector("body");
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "OLIVER FEHER is the champion";
+body.appendChild(newHeader)
