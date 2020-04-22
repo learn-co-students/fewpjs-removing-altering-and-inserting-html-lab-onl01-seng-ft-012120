5e08449955c4a386e72c7ad354e354754f93b844
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-22 15:31:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+newHeader.innerHTML ="YOUR-NAME is the champion";
